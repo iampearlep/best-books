2296da1452dfa8788f445283d72b89620e57cb6f
iampearlep <damilolaprincess4@gmail.com>
2021-07-21 17:54:45 +0100

    added one html, one js and one css file

---
+++ index.js
@@ -0,0 +1,9 @@
+const list = document.querySelector('#book-list ul');
+  //delete books
+list.addEventListener('click', function(e){
+
+    if(e.target.className == 'delete'){
+        const li =e.target.parentElement;
+        list.removeChild(li);
+    }
+})
